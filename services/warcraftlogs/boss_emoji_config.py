57e698c4989faf5a8365f65c7f0636438d6e16e3
Hekkipekki <dannor1992@gmail.com>
2026-07-11 02:39:29 +0200

    Add Siege of Orgrimmar boss emoji configuration

---
+++ services/warcraftlogs/boss_emoji_config.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+# Discord custom emojis uploaded for Siege of Orgrimmar bosses.
+# Keys are normalized encounter names. The lookup layer also supports aliases.
+BOSS_EMOJIS: dict[str, str] = {
+    "immerseus": "<:immerseus:1525293867740430487>",
+    "fallenprotectors": "<:fallenprotectors:1525293866574413934>",
+    "norushen": "<:norushen:1525293865207332914>",
+    "shaofpride": "<:shaofpride:1525293863693058218>",
+    "galakras": "<:galakras:1525293862002884688>",
+    "ironjuggernaut": "<:ironjuggernaut:1525293860547330128>",
+    "korkrondarkshaman": "<:korkrondarkshamans:1525293859523919902>",
+    "korkrondarkshamans": "<:korkrondarkshamans:1525293859523919902>",
+    "generalnazgrim": "<:generalnazgrim:1525293858328678471>",
+    "malkorok": "<:malkorok:1525293857200144534>",
+    "spoilsofpandaria": "<:spoils:1525293856118018148>",
+    "spoils": "<:spoils:1525293856118018148>",
+    "thokthebloodthirsty": "<:thok:1525293855111516280>",
+    "thok": "<:thok:1525293855111516280>",
+    "siegecrafterblackfuse": "<:siegecrafter:1525293853891100783>",
+    "siegecrafter": "<:siegecrafter:1525293853891100783>",
+    "paragonsoftheklaxxi": "<:paragons:1525293852926148768>",
+    "paragons": "<:paragons:1525293852926148768>",
+}
